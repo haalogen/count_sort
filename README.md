53995779102f56b19746813999b21d56076a1316
stan <citrus_82@nextmail.ru>
2015-10-12 12:50:49 +0300

    Added plan

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # count_sort
 Analysis of Counting Sort algorithm.
+
+Plan:
+1) count_sort function
+2) t(N) dependency
+3) num_collisions(k), k -- hash-table size
+
---
+++ analysis.py
@@ -0,0 +1,9 @@
+"""Analysis of Counting Sort algorithm."""
+import func as fu
+
+def main():
+    print "Counting Sort Analysis"
+    
+
+if __name__ == '__main__':
+    main()
